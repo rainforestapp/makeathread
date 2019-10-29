46a30271a3c33ff6c4a2c5ef9b1818a12d780368
Emanuel Evans <mail@emanuel.industries>
2019-10-29 15:17:37 -0700

    Heroku hacks

--- makeathread.rb
+++ makeathread.rb
@@ -1,4 +1,15 @@
 require 'slack-ruby-bot'
+require 'webrick'
+
+# Dumb server to keep Heroku happy
+server = WEBrick::HTTPServer.new :Port => ENV['PORT']
+server.mount_proc '/' do |req, res|
+  res.body = 'ok'
+end
+
+Thread.new do
+  server.start
+end
 
 class MakeAThead < SlackRubyBot::Bot
   match // do |client, data, match|
